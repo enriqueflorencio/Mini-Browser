e6ed645e9c749ba4b9ed7bf3ef1121dabf4d0ecb
Enrique Florencio <enriqueflorencio@enriques-macbook-pro.local>
2019-06-03 11:00:36 -0700

    created url request for the web view

--- hackingwithswiftproject4/hackingwithswiftproject4/ViewController.swift
+++ hackingwithswiftproject4/hackingwithswiftproject4/ViewController.swift
@@ -9,12 +9,24 @@
 //testing
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    //We need to setup our webView so that we can load it into our actual view
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
